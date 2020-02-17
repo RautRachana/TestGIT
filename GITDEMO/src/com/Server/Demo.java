305838e544c62207c6368e82315f067a4196d101
RautRachana <61134755+RautRachana@users.noreply.github.com>
2020-02-16 18:30:17 -1200

    Update Demo.java

--- GITDEMO/src/com/Server/Demo.java
+++ GITDEMO/src/com/Server/Demo.java
@@ -3,7 +3,7 @@ package com.Server;
 public class Demo {
 	public static void main(String[] args) {
 		System.out.println();
-		
+		System.out.println("hello");
 		System.out.println("GIT message print to server");
 		System.out.println("git program upload to server");
 	}
