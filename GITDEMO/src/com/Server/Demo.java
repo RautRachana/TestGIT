4e18adf3bdfa718979abd4e792ae557715967ed2
com <com@com-PC>
2020-02-17 11:38:38 -1200

    just changes normal in project

--- GITDEMO/src/com/Server/Demo.java
+++ GITDEMO/src/com/Server/Demo.java
@@ -3,6 +3,9 @@ package com.Server;
 public class Demo {
 	public static void main(String[] args) {
 		System.out.println();
+		
+		System.out.println("GIT message print to server");
+		System.out.println("git program upload to server");
 	}
 
 }
